a66db608a43b02e70133ebb5be16da5a2b943b20
Liam DeVoe <orionldevoe@gmail.com>
2021-05-01 12:57:25 -0400

    bump version to 0.0.12

--- terp.course.helper.user.js
+++ terp.course.helper.user.js
@@ -8,7 +8,7 @@
 // @include     https://app.testudo.umd.edu/soc/*
 // @grant       GM_xmlhttpRequest
 // @run-at      document-end
-// @version     0.0.11
+// @version     0.0.12
 // @description Integrate Rate My Professor to Testudo Schedule of Classes
 // @namespace   dkt.umdrmp.testudo
 // @require     https://unpkg.com/ajax-hook/dist/ajaxhook.min.js
